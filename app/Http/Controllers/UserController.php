ee645c9f762d87e83f3c16fe3ac2cd54af6b956d
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2023-11-11 20:10:56 +0200

    added all this

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+// app/Http/Controllers/UserController.php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function sayHello()
+    {
+        return 'Hello, Laravel!';
+    }
+}
+
